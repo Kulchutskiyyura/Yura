fa5e43e798567288dbf5e9f97715a8287cf52b45
User <kulchytskiyyura@gmail.com>
2020-01-04 16:42:25 +0200

    name ask

---
+++ name_ask/name_ask.py
@@ -0,0 +1,7 @@
+name=input("What is your name?: ")
+age=input("How old are you?: ")
+city=input("Where do you live?: ")
+print(f"your name is {name}")
+print(f"your age is {age}")
+print(f"you live in {city}")
+
