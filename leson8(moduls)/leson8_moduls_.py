71cc4436a1ad72d0d637e19c021aa56c04ad9f1b
User <kulchytskiyyura@gmail.com>
2020-01-26 00:13:01 +0200

    moduls

---
+++ leson8(moduls)/leson8_moduls_.py
@@ -0,0 +1,40 @@
+from math import pow
+from math import pi
+from random import randint
+
+number=randint(1,100)
+while(1):
+    guess=int(input("enter number: "))
+    if number>guess:
+        print("your number is lower")
+    elif guess>number:
+        print("your number is greater")
+    else:
+        print("you win")
+        break
+
+
+def find_circle_square(radius):
+    square=pi*pow(radius,2)
+    return square
+def find_rectangle_square(a_side,b_side):
+    return a_side*b_side
+def find_triangle_square(a_side,h):
+    print(sin(corner),corner)
+    return 0.5*h*a_side
+mes=input(" what do you whant to calculate cirscle or rectangle or triangle squere (c/r/t)?: ")
+if(mes=="c"):
+    radius=float(input("enter raius: "))
+    resalt=find_circle_square(radius)
+    print(resalt)
+elif mes=="r":
+    a_side=float(input("enter a side: "))
+    b_side=float(input("enter b side: "))
+    resalt=find_rectangle_square(a_side,b_side)
+    print(resalt)
+else:
+    a_side=float(input("enter a side: "))
+    h=float(input("enter b side: "))
+    
+    resalt=find_triangle_square(a_side,h)
+    print(resalt)
